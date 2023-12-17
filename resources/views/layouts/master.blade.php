7e4ced7dafd2ed3a3c19a489647d6a20f490d265
Scrollik <thedamirgaripov11@gmail.com>
2023-12-17 22:22:55 +0400

    first commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,6 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="{{ asset('css/mainstyle.css') }}" rel="stylesheet" >
     <title>NutTest</title>
 </head>
 <body>
@@ -26,11 +25,6 @@
                         <a class="nav-link active" aria-current="page" href="{{route('albumcreate.create')}}">Создать альбом</a>
                     </li>
                 @endauth
-{{--                @auth()--}}
-{{--                <li class="nav-item text-center">--}}
-{{--                    <a class="nav-link active" aria-current="page" href="{{route('album.index')}}">Создание альбома</a>--}}
-{{--                </li>--}}
-{{--                @endauth--}}
                  @guest()
                 <li class="nav-item ">
                     <a class="nav-link active" aria-current="page" href="{{route('login')}}">Войти</a>
