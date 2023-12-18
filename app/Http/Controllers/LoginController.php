ba26043561216842ddfc25f3b360ec36ff62aa2a
Scrollik <152374878+Scrollik@users.noreply.github.com>
2023-12-18 12:59:26 +0400

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
                 ]);
         }
         $request->session()->regenerate();
-        return redirect('album.index');
+        return redirect('/');
 
     }
     public function destroy (Request $request)
